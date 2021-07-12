c28307d141b6870c7e6f0713a3560f0f8561e15c
Marco Ortega <marco@tygonsoft.com>
2021-07-12 12:30:44 -0500

    Crea listado sobre tipos de datos en PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$entero = 12;
+$numnegativo = -145;
+$flotante = 3.1416;
+$cadenatxt = "cadena texto";
+$boleano = true;
+$boleano1 = false;
+$varno = null;
+
+print"<br>Tipo de dato numero entero: <br>".$entero;
+print"<br><br>Tipo de dato numero negativo: <br>".$numnegativo ;
+print"<br><br>Tipo de dato numero flotante: <br>".$flotante;
+print"<br><br>Tipo de dato cadena de texto: <br>".$cadenatxt;
+print"<br><br>Tipo de dato boleano verdadero: <br>".$boleano;
+print"<br><br>Tipo de dato boleano falso: <br>".$boleano1;
+print"<br><br> Tipo de dato Null".$varno;
+?>
